afad3aa0e7e9603a2bb138deaec628b517aad1c1
Gusian <hugo54136@hotmail.com>
2025-05-05 14:48:08 +0200

    exo

---
+++ exercice14.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class exercice14 {
+
+    public static void main(String[]args) {
+        
+        bool A;
+        
+        bool B;
+
+        bool C;
+
+        bool ea;
+
+        Scanner clavier = new Scanner(System.in);
+        
+        System.out.print("Donner la valeur de A :");
+        A=clavier.nextBoolean();
+        
+        System.out.print("Donner la valeur de B :");
+        B=clavier.nextBoolean();
+        
+        System.out.print("Donner la valeur de C :");
+        C=clavier.nextBoolean();
+
+        ea=A||B&&!C;
+        System.out.print("Résultat : "+ea);
+
+        clavier.close();
+    }
+}
+
+
+
+
+
+
+/* EXO14 : Avec PRINT & SCANNER : Tester l'expression algèbrique booléenne A OU B ET NON C
+ * 
+ * A , B ou C sont des booléens et ne peuvent être qu'égale à TRUE ou FALSE.
+ * 
+ * **********************AFFICHAGE ATTENDU ****************:
+ * 
+ * Donner une valeur A OU B ET NON C
+ * 
+ * FALSE
+ * TRUE
+ * FALSE
+ * 
+ * Résultat : TRUE
+ * 
+ * 
+ * 
+ * 
+ * 
+*/
+
+
